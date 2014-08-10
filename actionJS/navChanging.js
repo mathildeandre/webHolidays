9e27fb3f5aceb0c60a69e1fa447486a2991659c7
mathilde <mathilde.andre7@orange.fr>
2014-08-10 17:00:10 +0200

    evoluated

--- actionJS/navChanging.js
+++ actionJS/navChanging.js
@@ -1,11 +1,18 @@
 // Fonction modifiant le fond de page de façon aléatoire.
     function changeBackground() {
 	//parseInt(Math.random()*4) genere un nombre aleatoirement entre 0 et 3 inclus
-        window.document.getElementById("imgHeaderNav").style.background = 'url("../images/nav'+parseInt(Math.random()*4)+'.jpg") no-repeat';
+        window.document.getElementById("imgHeaderNav").style.background = 'url("../images/nav'+parseInt(Math.random()*4)+'.jpg") no-repeat, url("../images/papier-peint14.jpeg")';
         window.document.getElementById("imgHeaderNav").style.backgroundSize = '100% 300px';
+        window.document.getElementById("imgHeaderNav").style.backgroundAttachment="fixed";
  
         // Programmation d’un timer qui va réexécuter la fonction toutes les x secondes (en milliseconde)
-        window.setTimeout(changeBackground, 2000); // en millisecondes
+        window.setTimeout(changeBackground, 5000); // en millisecondes
     }
     // Mise en place du fond de page une première fois au chargement de la page. => important sinon ce ne marche pas ?
     window.onload = changeBackground;
+
+
+
+function changeCursor(id) {
+                document.getElementById(id).style.cursor='pointer';
+}
