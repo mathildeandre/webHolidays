e9ee57a07bd38d27b5f6d263de9a5ecaa1bc3b6e
mathilde <mathilde.andre7@orange.fr>
2014-08-11 23:54:45 +0200

    modif css

--- calculation/calculation.js
+++ calculation/calculation.js
@@ -202,3 +202,8 @@ function alternBackground(){
 		}
 	}
 }
+
+
+
+
+
--- doodle/doodle.js
+++ doodle/doodle.js
@@ -1,14 +1,71 @@
+   jQuery(document).ready(function(){
+   	jQuery('#debut').datepicker({
+	   dateFormat: 'dd/mm/yy'});
+   	jQuery('#fin').datepicker();
+   });
+
+/* declaration des variables */
+var theRowNumber = 1;
+var theColumnNumber = 1;
+var tabPerson=["emiel","matthijs","fabian","kiki","aina","mathilde"]; 
+var init = true;
 
 
 
-function conversion()
-{
-	var somme = prompt("Entrez la valeur en Euros :");
-	var resultat = somme * 6.55957;
-	alert(somme + " E\n" + resultat + " Frs");
+/* start */
+for(var i=0; i<tabPerson.length; i++){
+	addPersonne(tabPerson[i]);
+}
+//addRow()
+/* fin start */
+
+
+
+
+/****************************************** EXTRA PERSON *******************************************/
+
+function addPersonne(nameAdd){
+
+	var newRow = document.getElementById("tab").insertRow(-1);
+	var cell0 = newRow.insertCell(0);
+	cell0.innerHTML += "<b>"+nameAdd+"</b>";
+
+	var cell;
+	var id;
+	for(var i=1; i<=theColumnNumber; i++){
+		cell = newRow.insertCell(i);
+		id = theRowNumber + "1";
+		cell.innerHTML += '<input id="'+id+'" type="checkbox"> <label for="test2"></label>  '; 
+	}
+
 }
 
-conversion();
 
 
+/******************************** done ************************************/
+
+function reset(){
+	//window.location = "accueil3.html";//"file:///home/entdev3/Documents/GIT/web/accueil3.html"
+	location.href='calculation_holiday.html?tabPerson='+tabPerson;
+}
+
+function addRow(){
+	var newRow = document.getElementById("tab").insertRow(-1);
+	var cell0 = newRow.insertCell(0);
+	var id = theRowNumber + "1"; 
+	cell0.innerHTML += '<input id="'+id+'" type="text">'; 
+
+	var cell;
+	var id;
+	for(var i=1; i<=theColumnNumber; i++){
+		cell = newRow.insertCell(i);
+		id = theRowNumber + "1";
+		cell.innerHTML += '<input id="'+id+'" type="checkbox">'; 
+	}
+	theRowNumber++;
+}
+
+function addColumn(){
+
+}
 
