f73cfeb110126e05b1515a9322558eb7db7fdb57
mathilde <mathilde.andre7@orange.fr>
2014-08-12 20:03:40 +0200

    doodle quasi ok reste mise en forme

--- doodle/doodle.js
+++ doodle/doodle.js
@@ -1,12 +1,11 @@
-   jQuery(document).ready(function(){
-   	jQuery('#debut').datepicker({
-	   dateFormat: 'dd/mm/yy'});
-   	jQuery('#fin').datepicker();
-   });
+function functionDatepicker(){
+	jQuery('.datepick').datepicker({
+dateFormat: 'dd/mm/yy'});
+}
 
 /* declaration des variables */
 var theRowNumber = 1;
-var theColumnNumber = 1;
+var theColumnNumber = 2;
 var tabPerson=["emiel","matthijs","fabian","kiki","aina","mathilde"]; 
 var init = true;
 
@@ -32,7 +31,7 @@ function addPersonne(nameAdd){
 
 	var cell;
 	var id;
-	for(var i=1; i<=theColumnNumber; i++){
+	for(var i=1; i<theColumnNumber; i++){
 		cell = newRow.insertCell(i);
 		id = theRowNumber + "1";
 		cell.innerHTML += '<div class="roundedTwo"> <input type="checkbox" value="None" id="'+id+'" name="check"> <label for="'+id+'"></label> </div> '; 
@@ -51,22 +50,88 @@ function reset(){
 }
 
 function addRow(){
-	var newRow = document.getElementById("tab").insertRow(-1);
-	var cell0 = newRow.insertCell(0);
-	var id = theRowNumber + "1"; 
-	cell0.innerHTML += '<input id="'+id+'" type="text">'; 
+	var name = document.getElementById("name");
+	addPersonne(name.value);
+	/*
+	   var newRow = document.getElementById("tab").insertRow(-1);
+	   var cell0 = newRow.insertCell(0);
+	   var id = theRowNumber + "1"; 
+	   cell0.innerHTML += '<input id="'+id+'" type="text">'; 
 
-	var cell;
-	var id;
-	for(var i=1; i<=theColumnNumber; i++){
-		cell = newRow.insertCell(i);
-		id = theRowNumber + "1";
-		cell.innerHTML += '<input id="'+id+'" type="checkbox">'; 
-	}
-	theRowNumber++;
+	   var cell;
+	   var id;
+	   for(var i=1; i<=theColumnNumber; i++){
+	   cell = newRow.insertCell(i);
+	   id = theRowNumber + "1";
+	   cell.innerHTML += '<input id="'+id+'" type="checkbox">'; 
+	   }
+	   theRowNumber++;
+	 */
 }
 
 function addColumn(){
 
+	var dateRad = document.getElementById("date");
+	var textRad = document.getElementById("text");
+	var arrayLines = document.getElementById("tab").rows;
+
+		
+		
+		
+
+	if(textRad.checked==false && dateRad.checked==false){
+		alert("Veulliez cochez un des choix");
+	}
+	
+	else{
+	
+		
+		var tr = document.getElementById('tab').tHead.children[0];
+   	var th = document.createElement('th');
+	
+		if(textRad.checked){
+			var text = document.getElementById("textCol").value;
+			if(text==""){
+				alert("veulliez entrez un titre de colonne");
+			}else{
+				th.innerHTML = text;
+				tr.appendChild(th);
+			}
+		}
+		else if(dateRad.checked){
+			var dateS = document.getElementById("debutC").value;
+			var dateE = document.getElementById("finC").value;
+			if(dateS=="" || dateE==""){
+				alert("Selectionnez une date de début et une date de fin");
+			}else{
+				th.innerHTML = 'Du '+dateS+' au ' +dateE;
+				tr.appendChild(th);
+			}
+		}
+		var id;
+				for(i=1; i<theRowNumber; i++){
+					id = i + theColumnNumber;
+					var cell = arrayLines[i].insertCell(theColumnNumber);
+					cell.innerHTML += '<div class="roundedTwo"> <input type="checkbox" value="None" id="'+id+'" name="check"> <label for="'+id+'"></label> </div> '; 
+				}
+	}
+	theColumnNumber++;
 }
+
+function checkRadio(id){
+	if(id=="date"){
+		var text = document.getElementById("textCol");
+		text.value="titre colonne";
+	}
+	else if(id=="text"){
+			document.getElementById("debutC").value = "";
+			document.getElementById("finC").value = "";
+	}
+
+	var rad = document.getElementById(id);
+	rad.checked = true;
+}
+
+
+functionDatepicker();
 
