68dbceccf15ae725a249f3c34bed606edf03e925
fabiensauce <fabiensauce@orange.fr>
2014-09-25 15:02:05 +0200

    nettoyage CSS git st - Welcome page DONE !

--- DESIGN_folder/JS/actionJS/welcomeChanging.js
+++ DESIGN_folder/JS/actionJS/welcomeChanging.js
@@ -1,11 +1,13 @@
 
 // Fonction modifiant le fond de page de façon aléatoire.
     function changeBackground() {
+    	var elem = window.document.getElementById("bodyWelcome");
+    	
 	//parseInt(Math.random()*4) genere un nombre aleatoirement entre 0 et 3 inclus
-        //window.document.getElementById("imgHeaderNav").style.background = 'url("images/design'+parseInt(Math.random()*2)+'.jpg")';
-    	window.document.getElementById("imgHeaderNav").style.background = 'url("images/design0.jpg")';
-        //window.document.getElementById("imgHeaderNav").style.backgroundSize = '100% 100%';
-        window.document.getElementById("imgHeaderNav").style.backgroundAttachment="fixed";
+        //elem.style.background = 'url("images/design'+parseInt(Math.random()*2)+'.jpg")';
+    	elem.style.background = 'url("images/design0.jpg")';
+        //elem.style.backgroundSize = '100% 100%';
+        elem.style.backgroundAttachment="fixed";
  
         // Programmation d’un timer qui va réexécuter la fonction toutes les x secondes (en milliseconde)
         window.setTimeout(changeBackground, 5000); // en millisecondes
