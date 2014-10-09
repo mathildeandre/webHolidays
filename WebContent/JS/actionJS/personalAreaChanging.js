6776e7f2477601344c5de2fe73307ec7678802d7
fabiensauce <fabiensauce@orange.fr>
2014-10-09 18:50:42 +0200

    useless

---
+++ WebContent/JS/actionJS/personalAreaChanging.js
@@ -0,0 +1,18 @@
+//
+//// Fonction modifiant le fond de page de façon aléatoire.
+//    function changeBackground() {
+//    	var elem = window.document.getElementById("bodyPersonalArea");
+//    	
+//	//parseInt(Math.random()*4) genere un nombre aleatoirement entre 0 et 3 inclus
+//        //elem.style.background = 'url("images/design'+parseInt(Math.random()*2)+'.jpg")';
+//    	elem.style.background = 'rgba(255,255,255,0.1)','url("images/design6.jpg")';
+//        elem.style.backgroundSize = '100% 100%','100% 100%';
+//        elem.style.backgroundAttachment="fixed","fixed";
+// 
+//        // Programmation d’un timer qui va réexécuter la fonction toutes les x secondes (en milliseconde)
+//        window.setTimeout(changeBackground, 5000); // en millisecondes
+//    }
+//    // Mise en place du fond de page une première fois au chargement de la page. => important sinon ce ne marche pas ?
+//    window.onload = changeBackground;
+//
+//
